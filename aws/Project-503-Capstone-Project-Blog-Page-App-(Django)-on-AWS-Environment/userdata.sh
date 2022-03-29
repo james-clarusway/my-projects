1a447ec32784f32e98de86fd693ad28a713ccdc5
melekalan <dt.bmaslan@gmail.com>
2022-03-29 08:46:01 +0300

    capstone project updated

--- aws/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
+++ aws/Project-503-Capstone-Project-Blog-Page-App-(Django)-on-AWS-Environment/userdata.sh
@@ -3,13 +3,14 @@
 apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
-cd /home/ubuntu
-TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@github.com/MelekAlan/capstone-project.git
+cd /home/ubuntu/
+TOKEN="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+git clone https://$TOKEN@github.com/MelekAlan/capstone_project.git
+cd /home/ubuntu/capstone_project
 apt install python3-pip -y
-apt-get install python3.7-dev libmysqlclient-dev -y
+apt-get install python3.7-dev default-libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/capstone-project/src
+cd /home/ubuntu/capstone_project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
